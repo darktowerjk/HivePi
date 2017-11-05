5ad592feea7fe6e7bb193a55642939ab706c20c5
darktowerjk <darktowerjk@gmail.com>
2017-11-05 11:05:02 -0500

    First

--- runsensors.py
+++ runsensors.py
@@ -14,7 +14,6 @@ DEBUG = 1
 
 
 while True:
-
     humidity, temperature = Adafruit_DHT.read_retry(11, 4)
     print 'Temp: {0:0.1f} C  Humidity: {1:0.1f} %'.format(temperature, humidity)
     print 'before';
