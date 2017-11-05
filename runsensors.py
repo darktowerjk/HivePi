dbbfa65d00c18fe677ebb268533464261b2a7c80
darktowerjk <darktowerjk@gmail.com>
2017-11-05 12:39:58 -0500

    First

--- runsensors.py
+++ runsensors.py
@@ -40,7 +40,7 @@ def get_credentials():
     if not os.path.exists(credential_dir):
         os.makedirs(credential_dir)
     credential_path = os.path.join(credential_dir,
-                                   'client_secret.json')
+                                   'drive-python-quickstart.json')
 
     store = Storage(credential_path)
     print(credential_path)
