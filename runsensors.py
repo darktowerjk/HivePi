b58ec126ee43158270f58380df2e00c72aed44bd
darktowerjk <darktowerjk@gmail.com>
2017-11-05 11:45:42 -0500

    First

--- runsensors.py
+++ runsensors.py
@@ -62,7 +62,7 @@ def get_credentials():
 
 while True:
     humidity, temperature = Adafruit_DHT.read_retry(11, 4)
-    print 'Temp: {0:0.1f} C  Humidity: {1:0.1f} %'.format(temperature, humidity)
+    #print 'Temp: {0:0.1f} C  Humidity: {1:0.1f} %'.format(temperature, humidity)
     params = urllib.urlencode({'field1': temperature, 'field2': humidity, 'key': key})
     headers = {"Content-typZZe": "application/x-www-form-urlencoded", "Accept": "text/plain"}
     # store data in ThingSpeak
